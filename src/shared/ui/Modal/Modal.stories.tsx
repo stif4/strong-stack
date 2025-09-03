810ffb580c5646a6b199b1e440c37afd8dd3d187
amstry <vanva44@gmail.com>
2025-09-03 22:28:48 +0700

    add modal and portal

---
+++ src/shared/ui/Modal/Modal.stories.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Meta, StoryFn } from '@storybook/react';
+import { Modal } from 'shared/ui/Modal/Modal';
+
+export default {
+    title: 'shared/Modal',
+    component: Modal,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as Meta<typeof Modal>;
+
+const Template: StoryFn<typeof Modal> = (args) => <Modal {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    isOpen: true,
+    children:
+        'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.\n ',
+};
--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,16 +1,37 @@
 import { classNames } from 'shared/helpers/classNames/classNames';
 import cls from './Navbar.module.scss';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { useTranslation } from 'react-i18next';
+import { useCallback, useState } from 'react';
+import { Modal } from 'shared/ui/Modal/Modal';
 
 interface NavbarProps {
     className?: string;
 }
 
 export const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation();
+    const [isAuthModal, setIsAuthModal] = useState(false);
+
+    const onToggleModal = useCallback(() => {
+        setIsAuthModal((prev) => !prev);
+    }, []);
+
     return (
         <div className={classNames(cls.navbar, {}, [className])}>
-            <div className={cls.links}>
-                <></>
-            </div>
+            <Button
+                theme={ButtonTheme.CLEAR_INVERTED}
+                className={cls.links}
+                onClick={onToggleModal}
+            >
+                {t('Войти')}
+            </Button>
+            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
+                {}
+                {t(
+                    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid commodi consequatur eligendi impedit incidunt necessitatibus possimus quis saepe sunt totam.',
+                )}
+            </Modal>
         </div>
     );
 };
